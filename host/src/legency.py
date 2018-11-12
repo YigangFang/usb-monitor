7faf4dc99c23716e5a56c1c1daea8ac0e83b1414
tony <tao.zeng@rokid.com>
2018-11-12 20:21:33 +0800

    add support remote server

--- host/src/legency.py
+++ host/src/legency.py
@@ -1,29 +1,63 @@
 #!/usr/bin/python
-
 import hid
 import psutil
 import font
 import screen
 import style
+import sysinfo
+import json
+import sys
+import getopt
+import getpass
 
+hostname = ''; username = ''; passwd = ''
+#passopt
+try:
+    opts, args = getopt.getopt(sys.argv[1:],"hr:u:p:",["remote=", "user=", "passwd="])
+except getopt.GetoptError:
+    print 'legency.py -r hostname -u username -p password'
+    sys.exit(-1)
+
+for opt, arg in opts:
+    if opt == '-h':
+        print 'legency.py -r hostname -u username -p password'
+        sys.exit()
+    elif opt in ("-r", "--remote"):
+        hostname = arg
+    elif opt in ("-u", "--user"):
+        username = arg
+    elif opt in ("-p", "--passwd"):
+        passwd = arg
+
+#get username add passwd
+if hostname:
+    if not username:
+        username = raw_input("Remote User Name:")
+    if not passwd:
+        passwd =  getpass.getpass("password:")
+    try:
+        sysinfo.connect(hostname, username, passwd)
+    except Exception,e:
+        print(e)
+        exit(-1)
+    print('Sucess connect to host:' + hostname)
+
+#connect to usb hid device
 dev = hid.hid(0x0483, 0x5750)
 scr = screen.display(128,32)
-data = [0]*128
+
 while True:
     #get sytem info
-    bytes_recv = psutil.net_io_counters().bytes_recv
-    cpu=int(psutil.cpu_percent(1))
-    bytes_recv = psutil.net_io_counters().bytes_recv - bytes_recv
-    phymem = psutil.virtual_memory()
-    disk = int(psutil.disk_usage('/').percent)
-    #calculate 
-    mem = int(phymem.used * 100/phymem.total)
-    net = int(bytes_recv * 100/(10*1024*1024)) #10MByte 
-    #render
-    style.display_percent_bar(scr, 0 , 'CPU:', cpu)
-    style.display_percent_bar(scr, 1 , 'MEM:', mem)
-    style.display_percent_bar(scr, 2 , 'DSK:', disk)
-    style.display_percent_bar(scr, 3 , 'NET:', net)
+    try:
+        info = json.loads(sysinfo.info())
+        style.display_percent_bar(scr, 0 , 'CPU:', info['cpu'])
+        style.display_percent_bar(scr, 1 , 'MEM:', info['mem'])
+        style.display_percent_bar(scr, 2 , 'DSK:', info['disk'])
+        #scr.write(info['time'] + '\n')
+    except Exception,e:
+        print e
+        scr.clear()
+        scr.write(str(e))
     #display
     dev.write(scr.data())
 
